4fe36c31dc1d8a7731ff216fdc9f3b93709d39ce
asyi <asyisisis45@gmail.com>
2025-05-04 22:15:32 +0800

    add base64

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use PDO;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
@@ -33,6 +36,17 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
         }
 
+        if (env('DB_SSL_CERT_B64')) {
+            $certPath = storage_path('ssl-db-cert.pem');
+
+            // Decode and store cert content
+            File::put($certPath, base64_decode(env('DB_SSL_CERT_B64')));
+
+            // Force Laravel to use the SSL cert
+            Config::set('database.connections.mysql.options', [
+                PDO::MYSQL_ATTR_SSL_CA => $certPath,
+            ]);
+        }
         Schema::defaultStringLength(191);
 
         Validator::extend('phone_number', function ($attribute, $value, $parameters) {
